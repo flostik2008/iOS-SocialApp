a2924a108ae9f77e5d0933489de10c51951e4f66
Evgeny Vlasov <564n@Evgenys-MacBook-Pro.local>
2016-10-10 19:27:21 -0700

    Create custom UIViews for logIn view

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Evgeny Vlasov on 10/10/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
